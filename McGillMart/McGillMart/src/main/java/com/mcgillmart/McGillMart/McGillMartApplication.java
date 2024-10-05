1a156b8319c1a6286e767c0f39be92f664da3cd0
julia-b-grenier <julia.grenier@mail.mcgill.ca>
2024-10-04 20:23:29 -0400

    Upload Spring Boot application generated

---
+++ McGillMart/McGillMart/src/main/java/com/mcgillmart/McGillMart/McGillMartApplication.java
@@ -0,0 +1,13 @@
+package com.mcgillmart.McGillMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McGillMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McGillMartApplication.class, args);
+	}
+
+}
---
+++ McGillMart/McGillMart/src/test/java/com/mcgillmart/McGillMart/McGillMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcgillmart.McGillMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McGillMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
