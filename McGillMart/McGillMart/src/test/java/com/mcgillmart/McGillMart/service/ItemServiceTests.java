a1ddb5dd700722db153e4f76807bca5e87e6e7f7
fathisahar <sahar.fathi@mail.mcgill.ca>
2024-10-23 17:00:19 -0400

    the name test

--- McGillMart/McGillMart/src/test/java/com/mcgillmart/McGillMart/service/ItemServiceTests.java
+++ McGillMart/McGillMart/src/test/java/com/mcgillmart/McGillMart/service/ItemServiceTests.java
@@ -174,7 +174,6 @@ public class ItemServiceTests {
 
     public static final List<Item> ITEMS = new ArrayList<>();
 
-
     @Test
     public void testFilterItemsByName() {
         Item item1 = new Item();
@@ -187,7 +186,6 @@ public class ItemServiceTests {
         item1.setPrice(price1);
         item1.setCategory(Item.Category.valueOf(category1));
 
-
         Item item2 = new Item();
         String name2 = "aName2";
         String description2 = "aDescription2";
@@ -198,6 +196,53 @@ public class ItemServiceTests {
         item2.setPrice(price2);
         item2.setCategory(Item.Category.valueOf(category2));
 
+        Item item3 = new Item();
+        String name3 = "aName3";
+        String description3 = "aDescription3";
+        Integer price3 = 100;
+        String category3 = Category.Textbook.toString();
+        item3.setName(name3);
+        item3.setDescription(description3);
+        item3.setPrice(price3);
+        item3.setCategory(Item.Category.valueOf(category3));
+
+        ITEMS.add(item1);
+        ITEMS.add(item2);
+        ITEMS.add(item3);
+
+        when(itemDao.findAll()).thenReturn(ITEMS);
+
+        List<Item> items = service.filterItemsByName(item1.getName());
+
+        assertTrue(items.get(0).getName().equals(item1.getName()));
+        assertTrue(items.get(0).getDescription().equals(item1.getDescription()));
+        assertTrue(items.get(0).getPrice() == item1.getPrice());
+        assertTrue(items.get(0).getCategory().equals(item1.getCategory()));
+
+        
+    }
+
+    @Test
+    public void testFilterItemsByCategory() {
+        Item item1 = new Item();
+        String name1 = "aName1";
+        String description1 = "aDescription1";
+        Integer price1 = 100;
+        String category1 = Category.Clothing.toString();
+        item1.setName(name1);
+        item1.setDescription(description1);
+        item1.setPrice(price1);
+        item1.setCategory(Item.Category.valueOf(category1));
+
+        Item item2 = new Item();
+        String name2 = "aName2";
+        String description2 = "aDescription2";
+        Integer price2 = 100;
+        String category2 = Category.Clothing.toString();
+        item2.setName(name2);
+        item2.setDescription(description2);
+        item2.setPrice(price2);
+        item2.setCategory(Item.Category.valueOf(category2));
 
         Item item3 = new Item();
         String name3 = "aName3";
@@ -220,21 +265,6 @@ public class ItemServiceTests {
         for (Item item : clothing) {
             assertEquals(Item.Category.Clothing, item.getCategory());
         }
-
-    }
-
-    @Test
-    public void testFilterItemsByCategory() {
-
-
-        when(itemDao.findAll()).thenReturn(ITEMS);
-
-        List<Item> clothing = service.filterItemsByCategory(Item.Category.Clothing.toString());
-
-        for (Item item : clothing) {
-            assertEquals(Item.Category.Clothing, item.getCategory());
-        }
-
     }
 
 
