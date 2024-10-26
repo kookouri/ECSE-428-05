30f28d1159516728e115d2a5bed9f4d5cee71fc7
eric_zhu <eric.zhu@mail.mcgill.ca>
2024-10-26 15:35:58 -0400

    Migrate to Springboot 3.2.11

--- McGillMart/McGillMart/src/test/java/com/mcgillmart/McGillMart/features/CreateNewUserStepDefinitions.java
+++ McGillMart/McGillMart/src/test/java/com/mcgillmart/McGillMart/features/CreateNewUserStepDefinitions.java
@@ -1,6 +1,6 @@
 package com.mcgillmart.McGillMart.features;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
