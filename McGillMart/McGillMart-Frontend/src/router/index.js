57a091b56d657ae566a4e456bcd4cf5fed33b5c6
Tayba <tayba.jusab25@gmail.com>
2024-10-28 13:29:37 -0400

    Push frontend

--- McGillMart/McGillMart-Frontend/src/router/index.js
+++ McGillMart/McGillMart-Frontend/src/router/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import VueCookies from 'vue-cookies'
+import Home from '@/components/Home'
+import SignIn from '@/components/SignIn'
+import Registration from '@/components/Registration'
+import Settings from '@/components/Settings'
 
-Vue.use(Router)
+Vue.use(Router);
+Vue.use(VueCookies);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
+      path: "/",
+      name: "Home",
+      component: Home,
+    },
+    {
+      path: "/login",
+      name: "McGillMart Login page",
+      component: SignIn,
+    },
+    {
+      path: "/registration",
+      name: "Registration",
+      component: Registration,
+    },
+    {
+      path: "/settings",
+      name: "Settings",
+      component: Settings,
+    },
   ]
 })
+
