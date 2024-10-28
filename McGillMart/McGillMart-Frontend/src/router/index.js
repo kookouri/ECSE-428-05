8511bdbbeae09b1d60f89c5cba486305ab16a920
eric_zhu <eric.zhu@mail.mcgill.ca>
2024-10-28 15:20:21 -0400

    Disabled Authentication, View Profile Skeleton, and Bugfixes

--- McGillMart/McGillMart-Frontend/src/router/index.js
+++ McGillMart/McGillMart-Frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import ViewProfile from '@/components/ViewProfile'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/profile/view',
+      name: 'ViewProfile',
+      component: ViewProfile
     }
   ]
 })
---
+++ McGillMart/McGillMart/src/main/java/com/mcgillmart/McGillMart/WebConfiguration.java
@@ -0,0 +1,27 @@
+package com.mcgillmart.McGillMart;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebSecurity
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    	return http.httpBasic(HttpBasicConfigurer::disable).build();
+    }
+}
