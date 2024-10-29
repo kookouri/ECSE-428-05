4182bf4b57ea3541d77b31d36612904d000672ec
kookouri <97009104+kookouri@users.noreply.github.com>
2024-10-29 18:06:32 -0400

    ununsed import removal

--- McGillMart/McGillMart/src/test/java/com/mcgillmart/McGillMart/service/LoginServiceTest.java
+++ McGillMart/McGillMart/src/test/java/com/mcgillmart/McGillMart/service/LoginServiceTest.java
@@ -3,7 +3,6 @@ package com.mcgillmart.McGillMart.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
